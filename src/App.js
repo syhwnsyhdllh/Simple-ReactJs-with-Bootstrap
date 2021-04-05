17f3b4d7b1b41abb047a977fde51c22b6fc8a592
syhwnsyhdllh <cl09ay@gmail.com>
2021-04-05 22:03:39 +0800

    bootstrap &Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -7,20 +7,38 @@ class App extends Component {
   constructor(){
     super();
     this.state={
-      namaResto: 'Morningstar'
+      namaResto: 'Morningstar',
+      menuMakanan: [
+        {nama: 'Ayam bakar', harga: 25000, stock: 10},
+        {nama: 'Ikan Bakar', harga: 22000, stock: 0},
+      ]
     }
   }
-
+handleGantiNama = (nama) =>{
+  this.setState({namaResto: nama})
+}
   render() {
     return ( 
-     <div className="App">
+     <div className='App'>
        <h1 >Cafe and Resto {this.state.namaResto}</h1>
+       <button className="btn btn-primary" onClick={() => this.handleGantiNama ('Lucifer Morningstar')}>Ganti nama resto</button>
        <h2>Menu Makanan</h2>
-       <MenuMakanan namaMenu={"Ayam Bakar"} hargaMenu={25000}/>
-       <MenuMakanan namaMenu={"Ikan Bakar"} hargaMenu={15000}/>
+       {/* <MenuMakanan namaMenu={"Ayam Bakar"} hargaMenu={25000}/> */}
+       {/* <MenuMakanan namaMenu={"Ikan Bakar"} hargaMenu={15000}/> */}
+        <MenuMakanan
+          namaMenu={this.state.menuMakanan[0].nama}
+          hargaMenu={this.state.menuMakanan[0].harga}
+          stock={this.state.menuMakanan[0].stock}
+        />
+        <MenuMakanan
+          namaMenu={this.state.menuMakanan[1].nama}
+          hargaMenu={this.state.menuMakanan[1].harga}
+          stock={this.state.menuMakanan[1].stock}
+        />
+        
        <h2>Menu Minuman</h2>
        <MenuMinuman namaMenu={"Cold White"} hargaMenu={20000}/>
-       <MenuMinuman namaMenu={"Cold White"} hargaMenu={20000}/>
+       <MenuMinuman namaMenu={"Vietnam Drip"} hargaMenu={20000}/>
      </div>
      
     );
